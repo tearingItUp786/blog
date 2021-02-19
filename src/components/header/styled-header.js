d2d07219482e7470d6cbee085bd662d6d250118d
Taranveer Bains <taranveer786@gmail.com>
2021-02-19 12:44:32 -0800

    Mod: change logo size

--- src/components/header/styled-header.js
+++ src/components/header/styled-header.js
@@ -22,9 +22,9 @@ export const StyledHeader = styled.header`
 export const StyledLogo = styled.img`
   transition: transform 300ms ease-in-out;
   transition-delay: 300ms;
-  width: 65px;
-  transform: ${props =>
-    props.isFixed ? "scale(.6) translateX(-35%)" : "scale(1)"};
+  width: 50px;
+  transform: ${(props) =>
+    props.isFixed ? "scale(.8) translateX(-35%)" : "scale(1)"};
 `
 
 const translateTop = keyframes`
@@ -43,8 +43,8 @@ export const StyledNavContainer = styled.div`
   margin-right: auto;
   width: 100%;
   z-index: 10;
-  background: ${props => (props.isFixed ? "white" : "transparent")};
-  ${props =>
+  background: ${(props) => (props.isFixed ? "white" : "transparent")};
+  ${(props) =>
     props.isFixed
       ? {
           position: "fixed",
@@ -56,7 +56,7 @@ export const StyledNavContainer = styled.div`
             "0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)",
         }
       : { position: "relative", marginLeft: "auto", marginRight: "auto" }};
-  animation: ${props =>
+  animation: ${(props) =>
     props.isFixed
       ? css`
           ${translateTop} 300ms ease-in forwards 300ms
@@ -64,7 +64,7 @@ export const StyledNavContainer = styled.div`
       : ``};
 
   ${customMedia.lessThan("md")`
-      position: ${props => (props.isOpen && !props.isFixed ? "fixed" : "")};
+      position: ${(props) => (props.isOpen && !props.isFixed ? "fixed" : "")};
     `}
 `
 
@@ -79,7 +79,7 @@ export const StyledNav = styled.nav`
   transition: width 100ms ease 400ms;
   width: calc(${customMediaObject.xl} * 0.66);
   max-width: 90vw;
-  padding: ${props => (props.isFixed ? "0" : "0.5rem 0")};
+  padding: ${(props) => (props.isFixed ? "0" : "0.5rem 0")};
   ${customMedia.lessThan("md")`
     flex-wrap: nowrap;
     max-width: calc(100vw - 10vw);
@@ -136,8 +136,8 @@ export const LinksContainer = styled.div`
     width: 100%;
     background: transparent;
     transform: translateY(-300px);
-    top: ${props => (props.offsetTop ? `${props.offsetTop}px` : 0)};
-    animation: ${props =>
+    top: ${(props) => (props.offsetTop ? `${props.offsetTop}px` : 0)};
+    animation: ${(props) =>
       props.isOpen
         ? css`
             ${fadeIn} 300ms ease-in-out forwards 300ms
