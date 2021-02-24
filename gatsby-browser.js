701608d17c7cc0f1ce00899c8cc7b79778612aa3
Taranveer Bains <taranveer786@gmail.com>
2021-02-23 21:44:42 -0800

    Mod: some shit

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,9 @@
 // custom typefaces
 import React from "react"
 import GlobalProvider from "./src/components/global-provider"
+import "./src/css/font.css"
 import "prismjs/themes/prism-okaidia.css"
 import "prismjs/plugins/line-numbers/prism-line-numbers.css"
-
 export const wrapRootElement = ({ element }) => {
   return <GlobalProvider> {element} </GlobalProvider>
 }
