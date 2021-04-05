7ba6508de1510bc91b4096d30bb7f3018a701730
Taranveer Bains <taranveer786@gmail.com>
2021-04-04 20:36:19 -0700

    WIP: working on mortgages

---
+++ content/blog/7-canadian-mortgages/util.js
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+export const DefWrapper = styled.div`
+  h3 {
+    margin-bottom: 0;
+  }
+
+  p {
+    margin-top: 0.5rem;
+    margin-bottom: 2rem;
+  }
+`
