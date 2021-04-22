f32b47daf6136713304eb635878a5e7100aa53ed
Taranveer Bains <taranveer786@gmail.com>
2021-04-21 21:39:44 -0700

    Mod: add fonts

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,8 @@
 // custom typefaces
 import React from "react"
 import GlobalProvider from "./src/components/global-provider"
-import "@fontsource/lora"
+import "@fontsource/lora/700.css"
+import "@fontsource/lora/400.css"
 import "@fontsource/dm-sans"
 import "@fontsource/dm-serif-display"
 import "prismjs/themes/prism-okaidia.css"
