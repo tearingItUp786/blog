452d9968d4411bbd3bef579231e19687bf74d107
Taranveer Bains <taranveer786@gmailcom>
2023-01-09 10:19:30 -0800

    ye

--- content/til/2019-10-25-headless-ui-components/Example.jsx
+++ content/til/2019-10-25-headless-ui-components/Example.jsx
@@ -43,28 +43,23 @@ function PrettyToggle() {
         const appliedHandleClick = handleClick(console.log)
         return (
           <div className='relative block w-[120px] h-[56px]'>
-            <input
-              className={`opacity-0 w-0 h-0 checked:bg-[#ff00ff] focus:ring-1 focus:ring-[#2196f3] checked:translate-x-[62px]`}
-              type='checkbox'
-              checked={state.on}
-              onChange={appliedHandleClick}
-            />
-            <span
-              className={`
-          absolute
-          top-0 left-0 right-0 bottom-0 bg-gray-300 rounded-full transition duration-300'
-
-          before:absolute
-          before:rounded-full
-          before:content['']
-          before:h-[50px]
-          before:w-[50px]
-          before:left-[4px]
-          before:bottom-[4px]
-          before:bg-white
-          before:transition duration-300
-          `}
-            ></span>
+            <div
+              className={clsx(
+                '4 h-10 w-[5.5rem] flex items-center rounded-full p-2 cursor-pointer transition-colors bg-[#00000020] dark:bg-[#ffffff20]'
+              )}
+              onClick={appliedHandleClick}
+            >
+              <input
+                className={clsx(
+                  'drop-shadow-toggle transition-transform transition-color ease-in-out appearance-none cursor-pointer h-7 w-7 rounded-full',
+                  'bg-white translate-x-0',
+                  'dark:translate-x-[160%] dark:bg-gray-300'
+                )}
+                type='checkbox'
+                role='switch'
+                id='flexSwitchCheckDefault'
+              />
+            </div>
           </div>
         )
       }}
