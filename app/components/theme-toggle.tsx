e9abb7e9010335c2c7ff7379d97a873e69ccc249
Taranveer Bains <taranveer786@gmailcom>
2023-02-28 18:50:24 -0800

    set up intersection observer for the footer

--- app/components/theme-toggle.tsx
+++ app/components/theme-toggle.tsx
@@ -1,13 +1,46 @@
 import clsx from "clsx";
+import { useEffect, useState } from "react";
 import { Theme, useTheme } from "~/utils/theme-provider";
 
 const ThemeToggle = () => {
   const [, setTheme] = useTheme();
+  const [isFooterVisible, setIsFooterVisible] = useState(false);
+
+  console.log("isFooterVisible", isFooterVisible);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const cb: IntersectionObserverCallback = (entries) => {
+        if (entries?.[0]?.isIntersecting) {
+          setIsFooterVisible(true);
+        } else {
+          setIsFooterVisible(false);
+        }
+      };
+      const observer = new IntersectionObserver(cb, {
+        root: null,
+        rootMargin: "0px",
+        threshold: 0.1,
+      });
+
+      let footer = document.getElementsByTagName("footer")[0];
+      if (footer) {
+        observer.observe(footer);
+      }
+
+      return () => {
+        if (footer) {
+          observer.unobserve(footer);
+        }
+      };
+    }
+  }, []);
 
   return (
     <>
       <div
         className={clsx(
+          isFooterVisible && "absolute bottom-24",
           "z-100",
           "fixed bottom-4 right-4 h-10 w-[5.5rem] flex items-center rounded-full p-2 cursor-pointer transition-colors bg-[#00000020] dark:bg-[#ffffff20]"
         )}
