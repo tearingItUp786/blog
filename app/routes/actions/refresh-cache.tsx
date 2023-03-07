a35f168cf7bb5e987570573e28726aba8f538914
Taranveer Bains <taranveer786@gmailcom>
2023-03-07 14:11:25 -0800

    Yoloing stome stuff

--- app/routes/actions/refresh-cache.tsx
+++ app/routes/actions/refresh-cache.tsx
@@ -4,4 +4,6 @@ export const action: ActionFunction = async ({request}) => {
   if (request.headers.get('auth') !== process.env.REFRESH_CACHE_SECRET) {
     return redirect('https://youtu.be/VM3uXu1Dq4c')
   }
+
+  console.log('we got something buddy', request.body)
 }
--- other/refresh-content.js
+++ other/refresh-content.js
@@ -1,9 +1,75 @@
 const {getChangedFiles} = require('./get-changed-files')
+const hostname = 'localhost:3000'
+
+// try to keep this dep-free so we don't have to install deps
+function postRefreshCache({
+  http = require('https'),
+  postData,
+  options: {headers: headersOverrides, ...optionsOverrides} = {},
+}) {
+  return new Promise((resolve, reject) => {
+    try {
+      const postDataString = JSON.stringify(postData)
+
+      const options = {
+        hostname,
+        port: 443,
+        path: `/action/refresh-cache`,
+        method: 'POST',
+        headers: {
+          auth: process.env.REFRESH_CACHE_SECRET,
+          'Content-Type': 'application/json',
+          'Content-Length': Buffer.byteLength(postDataString),
+          ...headersOverrides,
+        },
+        ...optionsOverrides,
+      }
+
+      const req = http
+        .request(options, res => {
+          let data = ''
+          res.on('data', d => {
+            data += d
+          })
+
+          res.on('end', () => {
+            try {
+              resolve(JSON.parse(data))
+            } catch (error) {
+              reject(data)
+            }
+          })
+        })
+        .on('error', reject)
+      req.write(postDataString)
+      req.end()
+    } catch (error) {
+      console.log('oh no', error)
+      reject(error)
+    }
+  })
+}
 
 async function go() {
-  console.log('get changed files')
   const changes = await getChangedFiles('HEAD^', 'HEAD')
-  console.log(changes)
+  // with the changes, we can determine if we need to refresh the cache
+  // if there's nothing in the cache from content, we don't need to refresh the cache
+  // or update algolia
+
+  console.log('👀 checking for content changes')
+  let contentFiles = changes.filter(o => o.filename.indexOf('content') === 0)
+  if (!contentFiles.length) {
+    console.log('🤷 no content changes, exiting')
+    return
+  }
+
+  console.log('👍 content changes, refreshing cache and updating algolia')
+  // call the refresh cache function with the content files
+  await postRefreshCache({
+    postData: {
+      contentFiles,
+    },
+  })
   return changes
 }
 
