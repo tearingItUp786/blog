fff6a49a3ab4d9d9a0ce418b06663179a5c22e06
Taranveer Bains <taranveer786@gmailcom>
2023-03-07 19:17:40 -0800

    Yolo

--- app/routes/action/refresh-cache.tsx
+++ app/routes/action/refresh-cache.tsx
@@ -1,18 +1,25 @@
 import {ActionFunction, json, redirect} from '@remix-run/node'
+import {getMdxBlogListGraphql, getMdxTilListGql} from '~/utils/mdx'
 
-type Body = {contentFiles: Array<string>}
+type Body = {contentFiles: Array<{changeTypes: string; filename: string}>}
 
 export const action: ActionFunction = async ({request}) => {
   if (request.headers.get('auth') !== process.env.REFRESH_CACHE_SECRET) {
     return redirect('https://youtu.be/VM3uXu1Dq4c')
   }
 
-  const body = (await request.json()) as Body
+  const {contentFiles} = (await request.json()) as Body
 
-  if (!body.contentFiles) {
+  if (!contentFiles) {
     return json({ok: false})
   }
-  console.log('we got something buddy', body)
 
-  return json({ok: true})
+  // if we edited a content file, call the fetcher function for getContent
+  if (contentFiles.some(file => file.filename.startsWith('content/til/'))) {
+    await getMdxTilListGql()
+  }
+
+  if (contentFiles.some(file => file.filename.startsWith('content/blog/'))) {
+    await getMdxBlogListGraphql()
+  }
 }
