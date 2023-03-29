ae7abf3d203874e442f862eaed84798d2160f05a
Taranveer Bains <taranveer786@gmailcom>
2023-03-29 16:52:56 -0700

    update

--- app/utils/misc.tsx
+++ app/utils/misc.tsx
@@ -1,11 +1,12 @@
 import {format, parseISO} from 'date-fns'
 
 export function dateFormat(date: string) {
-  return new Date(date).toLocaleDateString('en-CA')
-  // return format(parseISO(date), 'MMMM dd, yyyy')
+  const [year, month, day] = date.split(/[-T]/)
+  return format(new Date(`${year}-${month}-${day}`), 'MMMM dd, yyyy')
 }
 
 export function dotFormattedDate(date: string) {
-  return new Date(date).toLocaleDateString('en-CA')
+  const [year, month, day] = date.split(/[-T]/)
+  return format(new Date(`${year}-${month}-${day}`), 'd.MM.yyyy')
   // return format(parseISO(date), 'd.MM.yyyy')
 }
