a2dd958a34e14d1b9a40fc6150a4a5d4dca1646a
Taranveer Bains <taranveer786@gmailcom>
2023-03-29 16:30:40 -0700

    updatez

--- app/utils/misc.tsx
+++ app/utils/misc.tsx
@@ -1,11 +1,11 @@
 import {format, parseISO} from 'date-fns'
 
 export function dateFormat(date: string) {
-  return date
+  return new Date(date).toLocaleDateString('en-CA')
   // return format(parseISO(date), 'MMMM dd, yyyy')
 }
 
 export function dotFormattedDate(date: string) {
-  return date
+  return new Date(date).toLocaleDateString('en-CA')
   // return format(parseISO(date), 'd.MM.yyyy')
 }
