7de363dda3eb264222ad089fa3c80700668c8e01
Taranveer Bains <taranveer786@gmailcom>
2023-03-29 17:21:26 -0700

    update

--- app/utils/misc.tsx
+++ app/utils/misc.tsx
@@ -1,11 +1,25 @@
-import {format, parseISO} from 'date-fns'
+import * as dateFns from 'date-fns'
 
-export function dateFormat(date: string) {
-  return date
-  // return format(parseISO(date), 'MMMM dd, yyyy')
+// export function dateFormat(date: string) {
+//   return date
+//   // return format(parseISO(date), 'MMMM dd, yyyy')
+// }
+//
+// export function dotFormattedDate(date: string) {
+//   return date
+//   // return format(parseISO(date), 'd.MM.yyyy')
+// }
+
+export function dateFormat(dateString: string) {
+  return dateFns.format(parseDate(dateString), 'MMMM dd, yyyy')
 }
 
-export function dotFormattedDate(date: string) {
-  return date
-  // return format(parseISO(date), 'd.MM.yyyy')
+export function dotFormattedDate(dateString: string) {
+  return dateFns.format(parseDate(dateString), 'd.MM.yyyy')
+}
+
+function parseDate(dateString: string) {
+  return dateFns.add(dateFns.parseISO(dateString), {
+    minutes: new Date().getTimezoneOffset(),
+  })
 }
