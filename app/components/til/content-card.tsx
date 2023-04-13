452bbfff9f784c2623e1a7fe03719029455a1a6c
Taranveer Bains <taranveer786@gmailcom>
2023-04-12 17:04:08 -0700

    Update to break word

--- app/components/til/content-card.tsx
+++ app/components/til/content-card.tsx
@@ -64,7 +64,7 @@ export const ContentCard = ({
           </NavLink>
         </div>
         <NavLink className="group no-underline" to={titleTo ?? `/til#${id}`}>
-          <H1 className="my-4 uppercase leading-[1em] transition-all group-hover:underline md:my-0">
+          <H1 className="my-4 break-words uppercase leading-[1em] transition-all group-hover:underline md:my-0">
             {title}
           </H1>
         </NavLink>
