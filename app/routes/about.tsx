4dda37da18a938683cf6d0c130404c77b70db8a9
Taranveer Bains <taranveer786@gmailcom>
2023-04-24 12:24:31 -0700

    ADd comma

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -68,7 +68,7 @@ export async function loader() {
 
 export const headers: HeadersFunction = ({}) => ({
   'Cache-Control':
-    'public max-age=60, s-maxage=60, stale-while-revalidate=2678400',
+    'public, max-age=60, s-maxage=60, stale-while-revalidate=2678400',
 })
 
 // need to fetch all content from the blog directory using github api
