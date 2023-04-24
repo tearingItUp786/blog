ecf8e1451fd8889bcba17694ab109f3d6fd6abb0
Taranveer Bains <taranveer786@gmailcom>
2023-04-24 12:03:03 -0700

    Chore: add new header to about page

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -1,6 +1,7 @@
 import {json} from '@remix-run/node'
 import {useLoaderData} from '@remix-run/react'
 import {scale} from '@cloudinary/url-gen/actions/resize'
+import type {HeadersFunction} from '@remix-run/node' // or cloudflare/deno
 
 import {BlockQuote, H3, H4, ShortQuote, TextLink} from '~/components/typography'
 // import { max } from "@cloudinary/url-gen/actions/roundCorners";
@@ -64,6 +65,10 @@ export async function loader() {
     mobileImage: mobileImage.toURL(),
   })
 }
+
+export const headers: HeadersFunction = ({}) => ({
+  'Cache-Control': 's-maxage=60, stale-while-revalidate=2678400',
+})
 
 // need to fetch all content from the blog directory using github api
 export default function About() {
