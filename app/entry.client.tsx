8da0e28aa22984196ceac3b59443e1095295a315
Taranveer Bains <taranveer786@gmailcom>
2023-06-16 17:49:43 -0700

    Revert "Update"
    This reverts commit 69297d224627cec1c9549b62cf5c29c5dec43067.

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -5,25 +5,24 @@ import {hydrateRoot} from 'react-dom/client'
 import * as Sentry from '@sentry/remix'
 import {useLocation, useMatches} from '@remix-run/react'
 
-process.env.NODE_ENV === 'production' &&
-  Sentry.init({
-    dsn: 'https://0357fa175ecd410aae09556246e163bc:4720935bcf404dffb8d516134afa125e@o4505072257138688.ingest.sentry.io/4505072258056192',
-    integrations: [
-      new Sentry.BrowserTracing({
-        routingInstrumentation: Sentry.remixRouterInstrumentation(
-          useEffect,
-          useLocation,
-          useMatches,
-        ),
-      }),
-      new Sentry.Replay(),
-    ],
-    // Performance Monitoring
-    tracesSampleRate: 0.1, // Capture 100% of the transactions, reduce in production!
-    // Session Replay
-    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-  })
+Sentry.init({
+  dsn: 'https://0357fa175ecd410aae09556246e163bc:4720935bcf404dffb8d516134afa125e@o4505072257138688.ingest.sentry.io/4505072258056192',
+  integrations: [
+    new Sentry.BrowserTracing({
+      routingInstrumentation: Sentry.remixRouterInstrumentation(
+        useEffect,
+        useLocation,
+        useMatches,
+      ),
+    }),
+    new Sentry.Replay(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 0.1, // Capture 100% of the transactions, reduce in production!
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+})
 
 function hydrate() {
   startTransition(() => {
