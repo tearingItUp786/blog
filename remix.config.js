a4a349a88652afd03011380fcfc50d54d77aebe0
Taranveer Bains <taranveer786@gmailcom>
2023-06-26 12:48:07 -0700

    Get it up with new remix and fix tailwind

--- remix.config.js
+++ remix.config.js
@@ -5,6 +5,7 @@ module.exports = {
   cacheDirectory: './node_modules/.cache/remix',
   // todo: change this to esm in v2
   serverModuleFormat: 'cjs',
+  tailwind: true,
   future: {
     v2_dev: true,
     v2_routeConvention: true,
