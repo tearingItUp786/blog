7a7f844191d320567aa5930ade13126cffb17887
Taranveer Bains <taranveer786@gmailcom>
2023-06-29 11:28:42 -0700

    Update about cache control

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -67,7 +67,8 @@ export async function loader() {
 }
 
 export const headers: HeadersFunction = ({}) => ({
-  'Cache-Control': 'public, s-maxage=60, stale-while-revalidate=2678400',
+  'Cache-Control':
+    'public, max-age=3600, s-maxage=86400, stale-while-revalidate=2678400',
 })
 
 // need to fetch all content from the blog directory using github api
