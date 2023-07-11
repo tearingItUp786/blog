975d113c2023b9df1fc306eebc430d0437552d2a
Taranveer Bains <taranveer786@gmailcom>
2023-07-10 19:31:32 -0700

    remove loader headers

--- app/routes/til.tsx
+++ app/routes/til.tsx
@@ -10,10 +10,7 @@ import styles from '~/styles/til.css'
 
 export async function loader() {
   const tilList = await getMdxTilListGql()
-  let headers = {
-    'Cache-Control': 'public,  s-maxage=3600, stale-while-revalidate=2678400',
-  }
-  return json({tilList}, {headers})
+  return json({tilList})
 }
 
 export const headers: HeadersFunction = ({loaderHeaders}) => {
