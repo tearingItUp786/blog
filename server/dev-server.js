5c712a228b1a91abd4066a7464ccacc88d7a744e
Taranveer Bains <taranveer786@gmailcom>
2023-07-12 22:24:22 -0700

    Feat: express server

---
+++ server/dev-server.js
@@ -0,0 +1,22 @@
+async function run() {
+  if (process.env.NODE_ENV === 'production') {
+    await import('../index.js')
+  } else {
+    const {execa} = await import('execa')
+    const command =
+      'tsx watch --clear-screen=false --ignore "app/**" --ignore "build/**" --ignore "node_modules/**" --inspect ./index.js'
+    execa(command, {
+      stdio: ['ignore', 'inherit', 'inherit'],
+      shell: true,
+      env: {
+        FORCE_COLOR: true,
+        MOCKS: true,
+        ...process.env,
+      },
+      // https://github.com/sindresorhus/execa/issues/433
+      windowsHide: false,
+    })
+  }
+}
+
+run()
