1b29e67bbd00de9d00a378c8265e49017c7f66f9
Taranveer Bains <taranveer786@gmailcom>
2023-07-13 16:41:01 -0700

    Revert "Feat: express server"
    This reverts commit 5c712a228b1a91abd4066a7464ccacc88d7a744e.

--- remix.config.js
+++ remix.config.js
@@ -3,12 +3,12 @@
  */
 module.exports = {
   cacheDirectory: './node_modules/.cache/remix',
-  serverPlatform: 'node',
+  // todo: change this to esm in v2
   serverModuleFormat: 'cjs',
   tailwind: true,
   future: {
     v2_dev: true,
     v2_routeConvention: true,
   },
-  serverDependenciesToBundle: [/^gsap.*/, /\@algolia.*/],
+  serverDependenciesToBundle: [/^gsap.*/],
 }
