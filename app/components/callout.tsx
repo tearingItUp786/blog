5f2b8c7a349ec907fdbcb7a7a7c9f5da77e0971e
Taranveer Bains <taranveer786@gmailcom>
2023-07-15 18:17:16 -0700

    Update callout

--- app/components/callout.tsx
+++ app/components/callout.tsx
@@ -23,10 +23,18 @@ export const Callout = ({type, title, description}: CalloutProps) => {
       )}
       role="alert"
     >
+      <svg
+        className="mr-3 inline h-4 w-4 flex-shrink-0"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+      >
+        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+      </svg>
       <span className="sr-only">Info</span>
       <div>
-        <span className="font-medium">{title}</span>
-        {description}
+        <span className="font-medium">{title}</span> {description}
       </div>
     </div>
   )
