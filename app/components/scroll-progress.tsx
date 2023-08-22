01c51f71d4dc4d318625a26f97dad1db6d4ad209
Taranveer Bains <taranveer786@gmailcom>
2023-08-22 14:32:25 -0700

    Chore: update loaders to look at request

--- app/components/scroll-progress.tsx
+++ app/components/scroll-progress.tsx
@@ -2,7 +2,6 @@ import {useLocation} from '@remix-run/react'
 
 export const ScrollProgress = () => {
   const loc = useLocation()
-  console.log(loc.pathname)
   const tilOrBlog = loc.pathname.match(/(blog\/.+|til)/)
   if (!tilOrBlog) return null
 
