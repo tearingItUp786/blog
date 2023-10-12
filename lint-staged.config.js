12857f30061a6cfe3ff4c2f1d6193e56fadca070
Taranveer Bains <taranveer786@gmailcom>
2023-10-11 21:00:22 -0700

    Chore: remove validate.sh command and use lint-staged

--- lint-staged.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  '*.+(js|jsx|json|yml|yaml|css|less|scss|ts|tsx|md|graphql|mdx|vue)': [
-    `npm run test --silent -- --watch=false`,
-    `npm run lint --silent`,
-    () => `npm run typecheck --silent`,
-    () => `npm run build --silent`,
-  ],
-}
