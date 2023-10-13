5fa6a444df0f2c1e98f06cedd2babdcbe8b4c95f
Taranveer Bains <taranveer786@gmailcom>
2023-10-12 21:40:07 -0700

    Chore: ad lint staged

---
+++ lint-staged.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  '*.+(js|jsx|json|yml|yaml|css|less|scss|ts|tsx|md|graphql|mdx|vue)': [
+    `npm run test --silent -- --watch=false`,
+    () => `npm run lint --silent`,
+    () => `npm run typecheck --silent`,
+    () => `npm run build --silent`,
+    `npm run clean --silent`,
+  ],
+}
