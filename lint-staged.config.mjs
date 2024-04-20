24d31d13170b5b6ba5a5becc5e4ee8aa82500a8d
Taranveer Bains <taranveer786@gmailcom>
2024-04-20 13:33:02 -0700

    fix lint staged

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,9 +1,10 @@
 export default {
-  '*.+(js|jsx|json|yml|yaml|css|less|scss|ts|tsx|md|graphql|mdx|vue)': [
+  '*.+(js|jsx|json|yml|yaml|css|less|scss|ts|tsx|mdx|md|graphql|mdx|vue)': [
     () => `npm run clean --silent`,
     `npm run test --silent -- --watch=false`,
     () => `npm run lint --silent`,
     () => `npm run typecheck --silent`,
+    () => `npm run format --silent`,
     () => `npm run build --silent`,
     () => `npm run clean --silent`,
   ],
