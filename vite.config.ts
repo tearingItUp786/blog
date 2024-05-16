1bd674f1e33b4a7063db8da4095c4503dd0e02b7
Taranveer Bains <taranveer786@gmailcom>
2024-05-16 14:34:12 -0700

    Update to allow sentry to get my source maps

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import {vitePlugin as remix} from '@remix-run/dev'
 import {installGlobals} from '@remix-run/node'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import {sentryVitePlugin} from '@sentry/vite-plugin'
 
 import {defineConfig} from 'vite'
 
@@ -17,6 +18,16 @@ export default defineConfig({
       serverBuildFile: 'index.mjs',
       serverModuleFormat: 'esm',
     }),
+    sentryVitePlugin({
+      org: process.env.SENTRY_ORG,
+      project: process.env.SENTRY_PROJECT,
+
+      // Auth tokens can be obtained from https://sentry.io/orgredirect/organizations/:orgslug/settings/auth-tokens/
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+    }),
     tsconfigPaths(),
   ],
+  build: {
+    sourcemap: true,
+  },
 })
