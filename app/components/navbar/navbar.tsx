3d5988f4cd6c6d8b05a6ca3d8a8206a33ac70d92
Taranveer Bains <taranveer786@gmailcom>
2024-05-16 11:26:01 -0700

    fix name missing on navbar

--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -51,11 +51,10 @@ export function Navbar() {
       <NavLink
         prefetch="intent"
         className={`
-    'transition-[color] normal text-body flex min-w-[150px] items-center justify-center pt-[5px] text-center text-xl duration-300 hover:!text-pink  focus:!text-pink  dark:text-white lg:bg-transparent lg:text-white lg:dark:text-gray-300
+    normal text-body flex min-w-[150px] items-center justify-center pt-[5px] text-center text-xl text-white transition-[color] duration-300  hover:!text-pink  focus:!text-pink  dark:text-black lg:bg-transparent lg:text-white lg:dark:text-gray-300
           `}
         to={`/?${searchParamsWithoutOffset}`}
       >
-        {/* <Logo className="py-1" /> */}
         Taran Bains
       </NavLink>
       <div
