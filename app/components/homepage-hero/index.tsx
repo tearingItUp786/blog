bcb8543529d08e6eb3af0a39b5f90fae8d2f7288
Taranveer Bains <taranveer786@gmailcom>
2024-05-15 17:12:58 -0700

    Update the sparkles

--- app/components/homepage-hero/index.tsx
+++ app/components/homepage-hero/index.tsx
@@ -9,10 +9,25 @@ import {OuterCircle} from './circle/outer-circle'
 import {InnerCircle} from './circle/inner-circle'
 import {BigStar} from './big-star'
 import {LineSvg} from './line'
+import Sparkles from './sparkles'
 
 export const HomepageHero = () => {
   return (
     <div className="relative min-h-full">
+      <div
+        className="
+        opacity-0
+        transition-all
+        dark:opacity-100
+        dark:delay-500
+        dark:duration-1000
+        "
+      >
+        <Sparkles
+          id="sparkles"
+          className="absolute left-[50%] top-[50%] w-[700px] "
+        />
+      </div>
       <div className="absolute left-1/2 top-1/2 h-[400px] w-[400px] -translate-x-1/2 -translate-y-[55%]">
         <div className="grid place-items-center">
           <OuterCircle />
@@ -29,17 +44,17 @@ export const HomepageHero = () => {
         </div>
       </div>
       {/* Star stuff */}
-      <BigStar className="dark:opacity-1 absolute bottom-0 max-w-[100px] opacity-0 opacity-0 transition-all dark:animate-[sparkle_infinite_4s_2s]" />
-      <BigStar className="dark:opacity-1 absolute bottom-0 right-[10%] max-w-[75px] opacity-0 opacity-0  transition-all dark:animate-[sparkle_infinite_4s_1.5s]" />
-      <BigStar className="dark:opacity-1 absolute right-[5%] top-0 max-w-[75px] opacity-0 opacity-0  transition-all dark:animate-[sparkle_infinite_5s_1.5s]" />
+      <BigStar className=" absolute bottom-0 max-w-[100px]  opacity-0 transition-all dark:animate-[sparkle_infinite_4s_2s]" />
+      <BigStar className=" absolute bottom-0 right-[10%] max-w-[75px]  opacity-0  transition-all dark:animate-[sparkle_infinite_4s_1.5s]" />
+      <BigStar className=" absolute right-[5%] top-0 max-w-[75px]  opacity-0  transition-all dark:animate-[sparkle_infinite_5s_1.5s]" />
 
       {/* Left Side */}
       <LineSvg id="line" className="absolute left-0 h-32 w-4" />
       <LineSvg id="line" className="absolute left-4 top-12 h-32 w-4" />
       <LineSvg id="line" className="absolute left-8 top-4 h-32 w-4" />
 
-      <BigStar className="dark:opacity-1 absolute left-[5%] top-0 max-w-[75px] opacity-0 transition-all dark:animate-[sparkle_infinite_5s_1.5s]" />
-      <BigStar className="dark:opacity-1 absolute left-[10%] top-[10%] max-w-[40px] opacity-0 transition-all  dark:animate-[sparkle_infinite_4s_1.5s]" />
+      <BigStar className=" absolute left-[5%] top-0 max-w-[75px] opacity-0 transition-all dark:animate-[sparkle_infinite_5s_1.5s]" />
+      <BigStar className=" absolute left-[10%] top-[10%] max-w-[40px] opacity-0 transition-all  dark:animate-[sparkle_infinite_4s_1.5s]" />
     </div>
   )
 }
