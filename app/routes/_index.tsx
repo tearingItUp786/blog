236ba42f608a36eda93b154e44cb0a7a86e8d92f
Taranveer Bains <taranveer786@gmailcom>
2024-07-12 00:24:21 -0700

    chore(animation): readd get request variable

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,10 @@
-import {HeadersFunction} from '@remix-run/node'
-import {type ShouldRevalidateFunctionArgs} from '@remix-run/react'
+import {LoaderFunctionArgs, json} from '@remix-run/node'
+import {
+  useLoaderData,
+  type ShouldRevalidateFunctionArgs,
+} from '@remix-run/react'
 import {HomepageHero} from '~/components/homepage-hero'
+import {H1, H2} from '~/components/typography'
 
 export function shouldRevalidate({
   currentUrl,
@@ -14,17 +18,22 @@ export function shouldRevalidate({
   return defaultShouldRevalidate
 }
 
-export const headers: HeadersFunction = ({}) => ({
-  'Cache-Control': 'public, max-age=2592000',
-})
+export const loader = ({request}: LoaderFunctionArgs) => {
+  let searchParams = new URLSearchParams(request.url.split('?')[1])
+  // new animation param
+  let animation = searchParams.get('animation')
+  return json({animation})
+}
 
 export default function Index() {
-  return (
-    <div
-      className="
+  const data = useLoaderData<typeof loader>()
+  if (data.animation !== null) {
+    return (
+      <div
+        className="
         dark: 
         relative 
-        h-[100lvh]
+        h-[calc(95vh_-_63.5px)]
         overflow-hidden 
         bg-gradient-to-br
         from-[#9E7ABE]
@@ -35,8 +44,37 @@ export default function Index() {
         bg-no-repeat
         text-center dark:from-[#35356B] dark:to-[#2F1A43]
       "
-    >
-      <HomepageHero />
+      >
+        <HomepageHero />
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <div
+        className={`
+      relative
+      h-[calc(95vh_-_63.5px)]
+      overflow-hidden
+      bg-contain
+      bg-fixed
+      bg-no-repeat
+      text-center
+     `}
+      >
+        <article className="absolute left-[50%] top-[50%] z-[2] translate-x-[-50%] translate-y-[-60%]">
+          <H1 className="text-body whitespace-nowrap text-[2.6rem] uppercase leading-[1.1] md:text-[5.2rem]">
+            Taran Bains
+          </H1>
+          <H2 className="bg-gray-100 px-2 text-center text-[1.5rem] uppercase leading-tight text-white dark:bg-accent md:text-[3.5rem]">
+            Tearing it up
+          </H2>
+          <H1 className="text-body whitespace-nowrap text-[1.25rem] uppercase leading-[1.5] md:text-[2.5rem]">
+            Like his life depends on it
+          </H1>
+        </article>
+      </div>
     </div>
   )
 }
