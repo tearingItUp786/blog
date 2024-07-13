97b6f3215a9624631bc844dd2a22f96c161951c6
Taranveer Bains <taranveer786@gmailcom>
2024-07-13 13:12:57 -0700

    chore(css): update lvh to svh

--- app/components/homepage-hero/index.tsx
+++ app/components/homepage-hero/index.tsx
@@ -24,7 +24,7 @@ export const HomepageHero = () => {
    */
   const [theme] = useTheme()
   return (
-    <div className="relative mx-auto min-h-lvh max-w-screen-xl">
+    <div className="relative mx-auto min-h-svh max-w-screen-xl">
       <div className="">
         <Sparkles
           id="sparkles"
