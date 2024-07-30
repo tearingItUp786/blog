b8217d61f727df299b546940be9693f7ea8304d1
Taranveer Bains <taranveer786@gmailcom>
2024-07-29 18:27:59 -0700

    chore: do the thing

--- app/routes/about/route.tsx
+++ app/routes/about/route.tsx
@@ -100,7 +100,7 @@ export default function About() {
 
   return (
     <div className="mx-auto mt-[2rem] min-h-[100svh] max-w-screen-xl pb-24">
-      <div className="ml-[8svw] mr-[8svw] max-w-full xl:mx-[4vw]">
+      <div className="max-w-full xl:mx-[4vw]">
         <FaceHero className="-mx-[15svw] md:mx-0" />
         <BlockQuote className="mx-auto mt-8 max-w-5xl" author="David Goggins">
           The only way that you’re ever going to get to the other side of this
