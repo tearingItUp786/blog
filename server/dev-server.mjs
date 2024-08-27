05c8c140dd5a26fbb97692e6955d0c4fc83c3453
Taranveer Bains <taranveer786@gmailcom>
2024-08-26 20:06:44 -0700

    chore: new TIL and update dev server

--- server/dev-server.mjs
+++ server/dev-server.mjs
@@ -1,5 +1,23 @@
 async function run() {
-  await import('../index.mjs')
+  if (process.env.NODE_ENV === 'production') {
+    await import('../index.mjs')
+  } else {
+    console.log('👀 Starting dev server')
+    const {execa} = await import('execa')
+    const command =
+      'tsx watch --clear-screen=false --ignore "vite**" --ignore "app/**" --ignore "build/**" --ignore "node_modules/**" --inspect ./index.mjs'
+    execa(command, {
+      stdio: ['ignore', 'inherit', 'inherit'],
+      shell: true,
+      env: {
+        FORCE_COLOR: true,
+        MOCKS: true,
+        ...process.env,
+      },
+      // https://github.com/sindresorhus/execa/issues/433
+      windowsHide: false,
+    })
+  }
 }
 
 run()
