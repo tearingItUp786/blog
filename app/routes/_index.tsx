6f16cf6723ac650bfa68ed0d8386567c1bc9a8ec
Taranveer Bains <taranveer786@gmailcom>
2024-08-27 21:27:23 -0700

    yolo

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -33,7 +33,7 @@ export default function Index() {
         className="
         dark: 
         relative 
-        h-[100%]
+        h-dvh
         overflow-hidden
         bg-gradient-to-br
         from-[#9E7ABE]
