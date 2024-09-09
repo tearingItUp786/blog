548eaf31d9bb3ac7b2eab302ba47d37577dbb7ab
Taranveer Bains <taranveer786@gmailcom>
2024-09-09 16:06:00 -0700

    chore(dev-server): update dev script

--- server/dev-server.mjs
+++ server/dev-server.mjs
@@ -11,10 +11,10 @@ async function run() {
     console.log('👀 Starting dev server')
     const {execa} = await import('execa')
 
-    const compileComand = `esbuild index.ts --outfile=index.cjs --platform=node  --format=cjs --target=es2020`
+    const compileComand = `esbuild index.ts --outfile=index.cjs --bundle  --platform=node  --format=cjs --target=es2020`
 
     console.log('🔨 Compiling mock files')
-    execa(compileComand, {
+    await execa(compileComand, {
       stdio: ['ignore', 'inherit', 'inherit'],
       cwd: 'mocks',
       shell: true,
@@ -27,20 +27,7 @@ async function run() {
       windowsHide: false,
     })
 
-    // this command just watches for changes and restarts the server
-    const command =
-      'tsx watch --clear-screen=false --ignore "vite**" --ignore "app/**" --ignore "build/**" --ignore "node_modules/**" --inspect ./index.mjs'
-    execa(command, {
-      stdio: ['ignore', 'inherit', 'inherit'],
-      shell: true,
-      env: {
-        FORCE_COLOR: true,
-        MOCKS: true,
-        ...process.env,
-      },
-      // https://github.com/sindresorhus/execa/issues/433
-      windowsHide: false,
-    })
+    await import('../index.mjs')
   }
 }
 
