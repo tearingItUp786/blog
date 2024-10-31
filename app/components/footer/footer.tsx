2fb4ba15c229f880a7fef384db7438dbc379c3a9
Taranveer Bains <taranveer786@gmailcom>
2024-10-30 17:18:43 -0700

    chore: another minor footer fix

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -7,36 +7,36 @@ export function Footer() {
   searchParamsWithoutOffset.delete('offset')
 
   return (
-    <footer className="mx-auto w-full max-w-screen-xl border-t-[1px]  bg-gray-100 px-4  py-6  dark:bg-white md:px-20 ">
-      <div className="mx-auto block w-full justify-between text-center lg:flex lg:text-left">
-        <span className="mb-4 block text-sm text-white dark:text-accent lg:mb-0">
+    <footer className="w-full  border-t-[1px] border-body  px-4  py-6 ">
+      <div className="mx-auto block w-full max-w-screen-xl justify-between text-center md:px-20 lg:flex lg:text-left">
+        <span className="mb-4 block text-sm text-body dark:text-accent lg:mb-0">
           Taran "tearing it up" Bains
         </span>
         <div className="flex flex-wrap md:block">
           <NavLink
             prefetch="intent"
-            className="basis-6/12 px-8 text-sm text-white underline dark:text-charcoal-gray"
+            className="basis-6/12 px-8 text-sm text-body underline "
             to={`/til?${searchParamsWithoutOffset}`}
           >
             TIL
           </NavLink>
           <NavLink
             prefetch="intent"
-            className="basis-6/12 px-8 text-sm text-white underline dark:text-charcoal-gray"
+            className="basis-6/12 px-8 text-sm text-body underline"
             to={`/about?${searchParamsWithoutOffset}`}
           >
             ABOUT
           </NavLink>
           <NavLink
             prefetch="intent"
-            className="basis-6/12 px-8 text-sm text-white underline dark:text-charcoal-gray"
+            className="basis-6/12 px-8 text-sm text-body underline "
             to={`/blog?${searchParamsWithoutOffset}`}
           >
             BLOG
           </NavLink>
           <NavLink
             prefetch="intent"
-            className="basis-6/12 px-8 text-sm text-white underline dark:text-charcoal-gray"
+            className="basis-6/12 px-8 text-sm text-body underline"
             to={`/uses?${searchParamsWithoutOffset}`}
           >
             USES
