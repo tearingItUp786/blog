ccda25d2ca57573aefd33f63d56e6fffff94dd16
Taranveer Bains <taranveer786@gmailcom>
2024-11-01 18:05:05 -0700

    chore: remove the icon for external links

--- app/utils/mdx.server.tsx
+++ app/utils/mdx.server.tsx
@@ -181,35 +181,6 @@ async function compileMdxForGraphql<
           [
             rehypeExternalLinks,
             {
-              content: () => {
-                return {
-                  type: 'element',
-                  tagName: 'svg',
-                  properties: {
-                    file: 'none',
-                    viewBox: '0 0 24 24',
-                    'stroke-width': '1.5',
-                    stroke: 'currentColor',
-                    class: 'size-5 ml-1',
-                  },
-                  children: [
-                    {
-                      type: 'element',
-                      tagName: 'path',
-                      properties: {
-                        class: 'inline',
-                        fill: 'none',
-                        strokeLinecap: 'round',
-                        strokeLinejoin: 'round',
-                        d: 'M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25',
-                      },
-                    },
-                  ],
-                }
-              },
-              properties: {
-                class: 'inline-flex items-center ',
-              },
               target: '_blank',
               rel: ['noopener'],
             },
