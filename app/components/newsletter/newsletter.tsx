db2426357622f177e4e271f9e68048f324603111
Taranveer Bains <taranveer786@gmailcom>
2025-03-09 17:52:40 -0700

    chore: update newsletter success messaging

--- app/components/newsletter/newsletter.tsx
+++ app/components/newsletter/newsletter.tsx
@@ -25,6 +25,8 @@ export const Newsletter = () => {
 		return null
 	}
 
+	const isLastResultSuccessful = lastResult?.status === 'success'
+
 	return (
 		<div className="mt-24 items-center gap-8 rounded-md border-[1.5px] border-solid border-border-color bg-transparent px-4 py-4 md:px-12 md:py-10 lg:flex">
 			<div className="flex basis-1/2 flex-wrap items-center justify-center md:flex-nowrap">
@@ -55,33 +57,30 @@ export const Newsletter = () => {
 						))}
 					</div>
 				) : null}
-				{lastResult?.status === 'success' && (
-					<h4
-						className="mb-8 flex items-start bg-transparent text-alert"
-						role="alert"
-					>
+				{isLastResultSuccessful && (
+					<div className="flex flex-wrap justify-center text-center">
 						<svg
 							xmlns="http://www.w3.org/2000/svg"
+							width={45}
+							height={36}
 							fill="none"
-							viewBox="0 0 24 24"
-							strokeWidth={1.5}
-							stroke="currentColor"
-							className="h-auto w-10"
 						>
 							<path
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+								fill="#262626"
+								d="M.436 32.036V12.23c.395-1.903 1.768-3.225 3.742-3.363 3.043-.211 18.42 0 18.42 0 1.096.1 1.194 1.428.164 1.721l-18.953.062-.272.158 15.324 15.33c.643.493 1.342.491 1.984 0l10.242-10.255-.037-.125c-1.696-1.195-2.83-3.133-3.24-5.156l-6.646-.029c-.999-.272-.887-1.594.15-1.705h6.33c.174-6.362 7.091-10.374 12.685-7.128 4.136 2.4 5.452 7.812 2.85 11.859-.94 1.464-2.262 2.485-3.813 3.247l-.012 14.97c-.215 1.947-1.685 3.424-3.655 3.577L4.01 35.389C2.29 35.261.642 33.757.435 32.032l.001.004ZM35.377 2.354c-5.258.54-7.855 6.847-4.457 10.951 2.928 3.536 8.466 3.147 10.883-.736 2.946-4.737-.924-10.779-6.426-10.215ZM2.245 32.207l10.153-10.043-10.11-10.106-.043 20.15Zm35.39-14.895c-1.69.398-3.328.03-4.935-.504l-5.306 5.357 9.981 10.002.173.041c-.04-.11.086-.253.086-.301V17.312ZM36.17 33.585 26.126 23.431c-2.445 1.758-4.677 6.558-8.252 4.125l-4.21-4.125L3.62 33.541l32.549.044Z"
 							/>
+							<circle className="fill-pink" cx={36.091} cy={9.036} r={8.446} />
 						</svg>
-						<span className="ml-1">
-							Success: Go check your inbox for the confirmation email 😎. Well,
-							what are you waiting for?
-							<strong className="text-xl"> Get to it!</strong>
-						</span>{' '}
-					</h4>
+						<h3 className="mb-2 mt-4 flex-[1_0_100%] font-display text-xl font-medium">
+							Awesome, you're subscribed!
+						</h3>
+						<p className="max-w-96 text-sm">
+							Check your inbox for the confirmation email. What are you waiting
+							for? Get to it 😎
+						</p>
+					</div>
 				)}
-				{lastResult?.status !== 'success' && (
+				{!isLastResultSuccessful && (
 					<fetcher.Form
 						method="post"
 						className="w-full flex-nowrap items-end justify-between gap-4 md:flex"
--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,9 +1,9 @@
 export default {
-  '*.+(js|jsx|json|yml|yaml|css|less|scss|ts|tsx|mdx|md|graphql|mdx|vue)': [
-    () => `npm run clean --silent`,
-    `pnpm run test --silent -- --watch=false`,
-    () => `pnpm run lint`,
-    () => `pnpm run typecheck`,
-    () => `prettier --write`,
-  ],
+	'*.+(js|jsx|json|yml|yaml|css|less|scss|ts|tsx|mdx|md|graphql|mdx|vue)': [
+		() => `npm run clean --silent`,
+		`pnpm run test --silent -- --watch=false`,
+		() => `pnpm run lint`,
+		() => `pnpm run typecheck`,
+		`prettier --write`,
+	],
 }
