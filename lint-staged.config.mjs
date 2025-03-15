b4e481fa7fd53c12b63eb5670fe374af49de7751
Taranveer Bains <taranveer786@gmailcom>
2025-03-15 09:43:41 -0700

    chore: only run lint on staged files

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -2,7 +2,7 @@ export default {
 	'*.+(js|jsx|json|yml|yaml|css|less|scss|ts|tsx|mdx|md|graphql|mdx|vue)': [
 		() => `npm run clean --silent`,
 		`pnpm run test --silent -- --watch=false`,
-		() => `pnpm run lint`,
+		`pnpm run lint`,
 		() => `pnpm run typecheck`,
 		`prettier --write`,
 	],
