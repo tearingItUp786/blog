f666e4cf56326d9e50c2e249edd6a874187f49c5
Taranveer Bains <taranveer786@gmailcom>
2025-04-11 20:01:12 -0700

    chore: fix padding issue with quote

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -76,7 +76,7 @@ export default function Index() {
 						</a>
 					</div>
 				</article>
-				<div className="mt-24 basis-full text-center lg:basis-2/3 lg:px-24">
+				<div className="mt-24 basis-full text-center lg:basis-2/3">
 					<H2 className="font-normal">{quoteData.quote}</H2>
 					<p className="mt-7 text-xl font-normal italic">{quoteData.author}</p>
 					<fetcher.Form method="POST">
