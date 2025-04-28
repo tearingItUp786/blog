111ec99c193768c4ec998acf33b613c9cb41c0c0
Taranveer Bains <taranveer786@gmailcom>
2025-04-28 10:41:11 -0700

    chore: remove hello world

--- app/routes/inngest.ts
+++ app/routes/inngest.ts
@@ -1,12 +1,11 @@
 // app/routes/api.inngest.ts
 import { serve } from 'inngest/remix'
 import { inngest } from '~/inngest/client'
-import { helloWorld } from '~/inngest/hello-world'
 import { refreshCache } from '~/inngest/refresh-cache'
 
 const handler = serve({
 	client: inngest,
-	functions: [helloWorld, refreshCache],
+	functions: [refreshCache],
 })
 
 export { handler as action, handler as loader }
