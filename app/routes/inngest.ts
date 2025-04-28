c53d1b49a1095e97f74f7c41e525124960d78f42
Taranveer (Taran) Bains <taranveer786@gmail.com>
2025-04-28 10:42:02 -0700

    chore: work on inngest (#147)
    Co-authored-by: Taranveer Bains <taranveer786@gmailcom>

---
+++ app/routes/inngest.ts
@@ -0,0 +1,11 @@
+// app/routes/api.inngest.ts
+import { serve } from 'inngest/remix'
+import { inngest } from '~/inngest/client'
+import { refreshCache } from '~/inngest/refresh-cache'
+
+const handler = serve({
+	client: inngest,
+	functions: [refreshCache],
+})
+
+export { handler as action, handler as loader }
