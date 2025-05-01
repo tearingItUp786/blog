60afcb4ae766b4cdd544daee9f9f286e207711d1
Taranveer Bains <taranveer786@gmailcom>
2025-05-01 09:07:31 -0700

    chore: minor update to lint staged

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,6 +1,6 @@
 export default {
 	'*.+(js|jsx|json|yml|yaml|css|less|scss|ts|tsx|mdx|md|graphql|mdx|vue)': [
-		() => `npm run clean --silent`,
+		() => `pnpm run clean --silent`,
 		`pnpm run test --silent -- --watch=false`,
 		`pnpm run lint`,
 		() => `pnpm run typecheck`,
