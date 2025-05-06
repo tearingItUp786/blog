e46fe1732a241899c76998de8790ce82fbe80d75
Taranveer Bains <taranveer786@gmailcom>
2025-05-05 17:47:04 -0700

    chore: update to move replaceContent into utils file

--- app/inngest/refresh-cache.ts
+++ app/inngest/refresh-cache.ts
@@ -12,6 +12,7 @@ import {
 import { redisClient } from '~/utils/redis.server'
 import { sendNtfyNotification } from '~/utils/ntfy'
 import PQueue from 'p-queue'
+import { replaceContent } from './utils'
 
 // Utility functions
 const P_QUEUE = new PQueue({ concurrency: 8 })
@@ -20,39 +21,6 @@ const ALGOLIA_INDEX = algoliaClient.initIndex('website')
 
 const cachifiedOptions = {
 	cachifiedOptions: { forceFresh: true },
-}
-
-function replaceContent(str = '') {
-	return (
-		str
-			// Strip MDX-specific content
-			?.replace(/import\s+.*?\s+from\s+['"].*?['"]/g, '') // Remove import statements
-			.replace(/import\s+{\s*.*?\s*}\s+from\s+['"].*?['"]/g, '') // Remove named imports
-			.replace(/export\s+default\s+.*?(?:\r\n|\r|\n|$)/g, '') // Remove export default statements
-			.replace(/export\s+const\s+.*?(?:;|$)/g, '') // Remove export const statements
-			.replace(/<.*?gss.*?>.*?<\/.*?>/g, '') // Remove GSS component tags
-			.replace(/<.*?GSS.*?>.*?<\/.*?>/g, '') // Remove GSS component tags (capitalized)
-			.replace(/<.*?Component.*?>.*?<\/.*?>/g, '') // Remove generic component tags
-			.replace(/{`.*?`}/g, '') // Remove template literals in braces
-			.replace(/{\/\*.*?\*\/}/gs, '') // Remove JSX comments
-			.replace(/{\/\/.*?(?:\r\n|\r|\n|$)}/g, '') // Remove single-line comments in braces
-			.replace(/{.*?}/g, '') // Remove remaining JSX expressions
-
-			// Original markdown stripping logic
-			.replace(/(<([^>]+)>)/gi, '') // Remove HTML tags
-			.replace(/!\[.*?\]\(.*?\)/g, '') // Remove image markdown
-			.replace(/\[(.*?)\]\(.*?\)/g, '$1') // Replace links with just the text
-			.replace(/(\*\*|__)(.*?)(\*\*|__)/g, '$2') // Remove bold formatting
-			.replace(/(\*|_)(.*?)(\*|_)/g, '$2') // Remove italic formatting
-			.replace(/(~~)(.*?)(~~)/g, '$2') // Remove strikethrough
-			.replace(/(?:\r\n|\r|\n|^)>.*(?:\r\n|\r|\n|$)/g, '') // Remove blockquotes
-			.replace(/(#{1,6}\s)(.*?)(\r\n|\r|\n)/g, '$2') // Remove headers
-			.replace(/(\r\n|\r|\n)\s*(\*|-|\+|[0-9]+\.)\s/g, '') // Remove list markers
-			.replace(/(\*\*|__|\*|_|~~)/g, '') // Remove any remaining markdown syntax
-			.replace(/```.*?```/gs, '') // Remove code blocks
-			.replace(/`.*?`/g, '') // Remove inline code
-			.trim()
-	) // Trim whitespace from the result
 }
 
 /**
