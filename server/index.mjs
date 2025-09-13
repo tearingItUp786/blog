e0f78ff1e0d5415070429304f37be226783d91c8
Taranveer Bains <taranveer786@gmailcom>
2025-09-13 10:48:55 -0700

    chore: introduce new rate limit

--- server/index.mjs
+++ server/index.mjs
@@ -68,10 +68,11 @@ const maxMultiple = process.env.NODE_ENV === 'development' ? 10_000 : 1
 
 app.use(
 	rateLimit({
-		windowMs: 60 * 1000 * 5, // 5 minutes,
-		limit: 1000 * maxMultiple, // limit each ip per window,
-		standardHeaders: true, // standard rate limit headers (none of the X-stuff)
-		legacyHeaders: false,
+		windowMs: 5 * 60 * 1000, // 5 minutes
+		limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+		standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+		legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+		ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
 	}),
 )
 
