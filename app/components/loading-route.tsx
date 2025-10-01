6610dc1e2c4c0b97c05cc8dcc214cab722eb7238
Taranveer Bains <taranveer786@gmailcom>
2025-09-30 21:30:26 -0700

    chore: update

--- app/components/loading-route.tsx
+++ app/components/loading-route.tsx
@@ -5,17 +5,17 @@ import { ToastUI } from './toast-ui'
 export const LoadingRoute = () => {
 	const navigation = useNavigation()
 	const loc = useLocation()
-	const [showLoadingComponent, setShowLoadingComponent] = useState(true)
+	const [showLoadingComponent, setShowLoadingComponent] = useState(false)
 	const { pathname } = navigation.location ?? {}
 
 	// This is a bit of a hack to get the loading component to show up
-	// after 2000 ms if the route has failed to change client side
+	// after 1000 ms if the route has failed to change client side
 	useEffect(() => {
 		let timeout: ReturnType<typeof setTimeout> | null = null
 		if (navigation.state === 'loading' && pathname !== loc.pathname) {
 			timeout = setTimeout(() => {
 				setShowLoadingComponent(true)
-			}, 2000)
+			}, 1000)
 		}
 
 		if (navigation.state === 'idle') {
