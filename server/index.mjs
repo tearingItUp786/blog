2993eae017ec717276de0396486358cc94614505
Taranveer Bains <taranveer786@gmailcom>
2025-10-22 21:11:10 -0700

    chore: update

--- server/index.mjs
+++ server/index.mjs
@@ -72,7 +72,7 @@ app.use(
 		limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
 		standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
 		legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-		ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+		ipv6Subnet: 60, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
 	}),
 )
 
