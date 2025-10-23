4c50410654cb855be92f5ae682330e3fe31dddcf
Taranveer Bains <taranveer786@gmailcom>
2025-10-22 21:13:10 -0700

    chore: update max multiple

--- server/index.mjs
+++ server/index.mjs
@@ -64,12 +64,12 @@ if (process.env.NODE_ENV === 'production') {
 	app.use(morgan('combined'))
 }
 
-const maxMultiple = process.env.NODE_ENV === 'development' ? 10_000 : 1
+const maxMultiple = process.env.NODE_ENV === 'development' ? 10_000 : 100
 
 app.use(
 	rateLimit({
 		windowMs: 5 * 60 * 1000, // 5 minutes
-		limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+		limit: maxMultiple, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
 		standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
 		legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
 		ipv6Subnet: 60, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
