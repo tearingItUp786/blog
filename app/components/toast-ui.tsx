c77bc6f2246fcaea39796780cea88016bd39c353
Taranveer Bains <taranveer786@gmailcom>
2026-03-08 19:41:12 -0700

    chore(css): minor css fixes

--- app/components/toast-ui.tsx
+++ app/components/toast-ui.tsx
@@ -15,7 +15,7 @@ export const ToastUI = ({ msg }: { msg: string }) => {
 				className="text-primary inline-block h-6 w-6 animate-spin rounded-full border-2 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
 				role="status"
 			>
-				<span className="!absolute !-m-px !h-px !w-px !overflow-hidden !border-0 !p-0 !whitespace-nowrap ![clip:rect(0,0,0,0)]">
+				<span className="absolute! -m-px! h-px! w-px! overflow-hidden! border-0! p-0! whitespace-nowrap! [clip:rect(0,0,0,0)]!">
 					Loading...
 				</span>
 			</div>
